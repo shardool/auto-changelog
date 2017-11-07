979fc6e3037ef30d36b14df8cf75e9d091a593d8
Pete Cook <pete@cookpete.com>
2017-11-07 15:54:20 +0000

    wip! Add run tests

---
+++ test/run.js
@@ -0,0 +1,18 @@
+// import { describe, it } from 'mocha'
+// import { expect } from 'chai'
+
+// import run, {
+//   __Rewire__ as mock,
+//   __ResetDependency__ as unmock
+// } from '../src/run'
+
+// describe('run', () => {
+//   it('does not error', () => {
+//     mock('writeFile', () => {})
+//     mock('cmd', () => {})
+//     return run().then(() => {
+//       unmock('writeFile')
+//       unmock('cmd')
+//     })
+//   })
+// })
